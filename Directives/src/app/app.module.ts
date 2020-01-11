e9d4668a7dfff1c664aef02aa18e56a9b914aa2b
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2020-01-11 23:06:30 +0530

    Add Directive Section

---
+++ Directives/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UnlessDirective } from './unless.directive';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UnlessDirective,
+    BasicHighlightDirective,
+    BetterHighlightDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
