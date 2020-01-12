9ff3ec32d2b51ee88573b3d4877326b98366e4ff
Pradhumansinh Padhiyar <pradhu619@gmail.com>
2020-01-13 00:08:30 +0530

    Add Service Section

---
+++ Service/src/app/new-account/new-account.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+  // providers: [LoggingService]
+})
+export class NewAccountComponent implements OnInit {
+
+  constructor(private loggingService: LoggingService,
+    private accountsService: AccountsService) {
+this.accountsService.statusUpdated.subscribe(
+(status: string) => alert('New Status: ' + status)
+);
+}
+
+onCreateAccount(accountName: string, accountStatus: string) {
+this.accountsService.addAccount(accountName, accountStatus);
+// this.loggingService.logStatusChange(accountStatus);
+}
+
+  ngOnInit() {
+  }
+
+}
